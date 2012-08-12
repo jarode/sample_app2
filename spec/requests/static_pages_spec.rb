57f64ace6602770175621c804a4a7ace1375b94c
jarode <jarode2@o2.pl>
2012-08-12 18:31:48 +0100

    desc - jPortal added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -71,7 +71,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "sample app"
+    click_link "jPortal"
     page.should have_selector 'h1', text: 'Sample App'
   end
 end
